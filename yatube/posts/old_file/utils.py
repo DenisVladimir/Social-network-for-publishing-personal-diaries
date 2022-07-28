bc7ece9420cd115e7efded37f2f1f0d330a07ee3
Денис Дятликович <statuspobeda@yandex.ru>
2022-07-29 00:04:34 +0300

    Commit 6 final 3

--- yatube/posts/old_file/utils.py
+++
@@ -1,8 +0,0 @@
-from django.core.paginator import Paginator
-
-
-def my_paginator(request, post_list, NUM_OF_POSTS):
-    pagin = Paginator(post_list, NUM_OF_POSTS)
-    page_number = request.GET.get('page')
-    page_obj = pagin.get_page(page_number)
-    return page_obj
